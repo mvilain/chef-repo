cdd81b7c9211afda902cd3b34d789cfdbadc2afa
Michael Vilain <michael@vilain.com>
2018-07-22 13:00:09 -0700

    remove depricated database cookbook requirement

--- cookbooks/phpapp/metadata.rb
+++ cookbooks/phpapp/metadata.rb
@@ -1,6 +1,6 @@
 name             'phpapp'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'none'
+maintainer_email 'none'
 license          'All rights reserved'
 description      'Installs/Configures phpapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -9,5 +9,4 @@ version          '0.1.0'
 depends "apache2"
 depends "mysql"
 depends "php"
-depends "database"
 depends "mysql2_chef_gem"
