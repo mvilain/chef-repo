1ca60b4a0b0d1dbd9a6286eb42ac08f14617c522
Michael Vilain <michael@vilain.com>
2018-07-22 20:12:35 -0700

    fix nodename

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "mev_work"
+node_name                "mvilain"
 client_key               "#{current_dir}/mvilain.pem"
 chef_server_url          "https://api.chef.io/organizations/mvilain"
 cookbook_path            ["#{current_dir}/../cookbooks"]
